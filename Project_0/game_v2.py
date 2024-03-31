ab185e4e8e9ff31d42bd0c05032d3ee73c97befd
Stromont <Stromont@users.noreply.github.com>
2024-03-31 18:17:54 +0700

    first try do homework

--- Project_0/game_v2.py
+++ Project_0/game_v2.py
@@ -24,26 +24,7 @@ def random_predict(number: int = 1) -> int:
     return count
 
 
-def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
-
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
-    """
-    count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
-
-    for number in random_array:
-        count_ls.append(random_predict(number))
-
-    score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
-    return score
-
+ся
 
 if __name__ == "__main__":
     # RUN
---
+++ Project_0/game_v3.py
@@ -0,0 +1,108 @@
+"""Игра угадай число
+Компьтер загадывает и угадывает число
+"""
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+
+    return count
+
+
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    return score
+
+def game_core_v3(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число и усанавливаем интервал возможных значений
+       в зависимости от того больше или меньше загаданное число к рандомному изменяем интервал
+       и устанавливаем новое число в серидину нового интервала
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+    max = 101
+    min = 0
+    while number != predict:
+        count += 1
+        if number > predict:
+            min = predict
+            predict += (max-predict) // 2
+        elif number < predict:
+            max = predict
+            predict -= (predict - min) // 2
+
+    return count
+
+
+#Run benchmarking to score effectiveness of all algorithms
+print('Run benchmarking for random_predict: ', end='')
+score_game(random_predict)
+
+print('Run benchmarking for game_core_v2: ', end='')
+score_game(game_core_v2)
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
